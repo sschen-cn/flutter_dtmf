87900dfe73600c5612bab9df916608caeea1a834
corey <corey.yan@justalk.com>
2020-02-06 13:04:51 +0800

    feature: 回退适配到flutter sdk v1.9.1+hotfix.6

--- example/android/app/src/main/java/com/dormmom/flutter_dtmf_example/MainActivity.java
+++ example/android/app/src/main/java/com/dormmom/flutter_dtmf_example/MainActivity.java
@@ -1,13 +1,19 @@
 package com.dormmom.flutter_dtmf_example;
 
-import androidx.annotation.NonNull;
-import io.flutter.embedding.android.FlutterActivity;
-import io.flutter.embedding.engine.FlutterEngine;
+import android.os.Bundle;
+
+import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
-  @Override
-  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
-    GeneratedPluginRegistrant.registerWith(flutterEngine);
-  }
+//  @Override
+//  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+//    GeneratedPluginRegistrant.registerWith(flutterEngine);
+//  }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        GeneratedPluginRegistrant.registerWith(this);
+    }
 }
